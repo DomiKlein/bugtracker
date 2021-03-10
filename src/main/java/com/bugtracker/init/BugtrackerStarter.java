49a23e4234dda0c2f8b06f103d39a3489b2a543d
Dominik Klein <48219921+DomiKlein@users.noreply.github.com>
2021-03-10 03:08:45 +0100

    Switched from manual Tomcat to embedded Jetty server setup

--- src/main/java/com/bugtracker/init/BugtrackerStarter.java
+++ src/main/java/com/bugtracker/init/BugtrackerStarter.java
@@ -1,16 +1,69 @@
 package com.bugtracker.init;
 
-import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 import com.bugtracker.database.DatabaseConnection;
 
+// TODO : Add comment
 public class BugtrackerStarter {
 
-	public static void main(String[] args) {
-		// Configure logger
-		BasicConfigurator.configure();
+	/** The used logger. */
+	private static final Logger LOGGER = LogManager.getLogger(BugtrackerStarter.class);
 
+	/** The name of the package which contains all service classes. */
+	private static final String SERVICES_PACKAGE_NAME = "com.bugtracker.services";
+
+	public static void main(String[] args) {
+		// Configure database connection
 		DatabaseConnection databaseConnection = new DatabaseConnection();
-		databaseConnection.run();
+		// TODO : Uncomment
+		// databaseConnection.run();
+
+		startServer();
+	}
+
+	// TODO : Add comment
+	private static void startServer() {
+		ServletContextHandler servletContext = prepareServletContext();
+		WebAppContext webAppContext;
+
+		HandlerCollection handlerCollection = new HandlerCollection();
+		handlerCollection.setHandlers(new Handler[]{servletContext});
+
+		Server jettyServer = new Server(8080);
+		jettyServer.setHandler(handlerCollection);
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		} catch (Exception e) {
+			LOGGER.error("Error while trying to run the webserver");
+			jettyServer.destroy();
+		}
+	}
+
+	// TODO : Add comment
+	private static ServletContextHandler prepareServletContext() {
+		ServletContextHandler servletContext = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		servletContext.setContextPath("/api");
+
+		ServletHolder jerseyServlet = servletContext.addServlet(org.glassfish.jersey.servlet.ServletContainer.class,
+				"/*");
+		jerseyServlet.setInitOrder(0);
+		jerseyServlet.setInitParameter("jersey.config.server.provider.packages", SERVICES_PACKAGE_NAME);
+
+		return servletContext;
+	}
+
+	// TODO : Add comment
+	private static WebAppContext prepareWebAppContext() {
+		// TODO : Implement and use in startWebserver()
+		return null;
 	}
 }
--- src/main/java/com/bugtracker/services/HelloWorldService.java
+++ src/main/java/com/bugtracker/services/HelloWorldService.java
@@ -1,8 +1,5 @@
 package com.bugtracker.services;
 
-
-import com.bugtracker.services.utils.JsonString;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -11,9 +8,9 @@ import javax.ws.rs.core.MediaType;
 @Path("/info")
 public class HelloWorldService {
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public JsonString sayHelloWorld() {
-        return new JsonString("Hello World");
-    }
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHelloWorld() {
+		return "Hello World";
+	}
 }
